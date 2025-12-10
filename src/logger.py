185e4e13aab1cd45d787bf1d25439b500dc2449d
Sai kishan <saikishan2452003@gmail.com>
2025-12-10 17:16:08 +0530

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,32 @@
+import logging
+import sys
+
+# Configure logging (THIS CREATES THE FILE)
+logging.basicConfig(
+    filename="application.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+def error_message_detail(error, error_detail: sys):
+    _, _exc_tb = error_detail.exc_info()
+    file_name = _exc_tb.tb_frame.f_code.co_filename
+
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, _exc_tb.tb_lineno, str(error)
+    )
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(
+            error_message, error_detail=error_detail
+        )
+
+    def __str__(self):
+        return self.error_message
+
+
+
+ 
