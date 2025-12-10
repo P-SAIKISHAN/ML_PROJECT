cf38aee3bfcb3045b4c5abae44354f65802dfe55
Sai kishan <saikishan2452003@gmail.com>
2025-12-10 17:34:57 +0530

    logger

--- src/logger.py
+++ src/logger.py
@@ -1,32 +1,17 @@
 import logging
-import sys
+import os
+from datetime import datetime
 
-# Configure logging (THIS CREATES THE FILE)
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
 logging.basicConfig(
-    filename="application.log",
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"
+
+
 )
-
-def error_message_detail(error, error_detail: sys):
-    _, _exc_tb = error_detail.exc_info()
-    file_name = _exc_tb.tb_frame.f_code.co_filename
-
-    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, _exc_tb.tb_lineno, str(error)
-    )
-    return error_message
-
-class CustomException(Exception):
-    def __init__(self, error_message, error_detail: sys):
-        super().__init__(error_message)
-        self.error_message = error_message_detail(
-            error_message, error_detail=error_detail
-        )
-
-    def __str__(self):
-        return self.error_message
-
-
-
- 
